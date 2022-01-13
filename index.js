f42cfd19904ba3581f1bfd82a27f12674be4ff0c
ravi0the0sun <28498241+ravi0the0sun@users.noreply.github.com>
2022-01-13 15:20:56 +1100

    added the fetch function in login

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function login() {
+async function login() {
 	const userName = document.getElementById('loginInput').value.toString();
-	
+	try {
+		const userInfo = await fetch('http://localhost:7777/api/users', {
+			method: 'GET',
+		});
+		console.log(userInfo);
+	} catch (error) {
+		console.log(error);
+	}
 }
